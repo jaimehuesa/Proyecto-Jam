954f78d171afbdccaa47ee0c4399991714e9976d
jaimehuesa <jaime.huesa@gmail.com>
2018-05-19 01:44:22 +0200

    Seleccion de Solucion

---
+++ Assets/Scripts/ResourceSelection/ResourceSelection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceSelection : MonoBehaviour {
+	public GameObject[] solutions;
+	private GameObject currentSolution;
+	// Use this for initialization
+	void Start () {
+		currentSolution = solutions [0];
+		Debug.Log ("Primera solucion seleccionada");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("1")){
+			currentSolution = solutions [0];
+			Debug.Log("Primera solucion seleccionada");
+		}
+		if(Input.GetKey("2")){
+			currentSolution = solutions [1];
+			Debug.Log("Segunda solucion seleccionada");
+		}
+		if(Input.GetKey("3")){
+			currentSolution = solutions [2];
+			Debug.Log("Tercera solucion seleccionada");
+		}
+
+	}
+}
