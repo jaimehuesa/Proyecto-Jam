61895ce0779d66898acf0f1f4384fe5acc5c411d
jaimehuesa <jaime.huesa@gmail.com>
2018-05-19 01:06:52 +0200

    PointClick Script

---
+++ Assets/Scripts/ChangeCells/ScreenPointToWorld.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenPointToWorld : MonoBehaviour {
+	public Camera camara;
+	private Ray ray;
+	private RaycastHit hitInfo;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+
+			ray = camara.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hitInfo, 100f)){
+				if (hitInfo.collider.CompareTag ("Obstacle")) {
+					Debug.Log ("Cambiar obstaculo");
+				} else {
+					Debug.Log ("No puedes hacer nada");
+				}
+			}
+
+		}
+	}
+}
