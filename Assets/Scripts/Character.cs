6b4b6df4430c181aee08eae2b8eea89eba5c1afc
PabloPortatil <trapinel>
2018-05-19 13:32:55 +0200

    CharacterMovementClean

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,86 +2,86 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character : MonoBehaviour {
-    public enum disabilites {blind,alzheimer,wheelchair};
-    public disabilites myDisability;
+public class Character : MonoBehaviour
+{
+    public enum disabilites { blind, alzheimer, wheelchair };
+    enum actionsToDo { moveAction, rotationAction, disappear };
+    actionsToDo myCurrentAction = actionsToDo.moveAction; // por defecto va a moverse
+    disabilites myDisability;
     Vector3 startPosition3D;
     Vector3 destinyPosition3D;
     float moveDistance = 1;
     float moveCharacterTime = 1;
     float moveCharacterTimer = 0;
     bool startMoveCounter = false;
-    /*public float speed=1;
-    public int xPosInMap=0;
-    public int yPosInMap =0;
-    // Use this for initialization
 
-    
+    float rotateYValue = -90;
 
     GameObject myCell;
-    GameObject nextCell;*/
+    GameObject nextCell;
+    void Start()
+    {
+        myDisability = disabilites.blind;
+    }
+    public disabilites getMyDisability()
+    {
+        return myDisability;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (startMoveCounter)
+        {
+            switch (myCurrentAction)
+            {
+                case actionsToDo.moveAction:
+                    move(); break;
+                case actionsToDo.rotationAction: rotate();  break;
+            }
+            moveCharacterTimer += Time.deltaTime;
+        }
 
-    /*public void setCellInfo(GameObject a_myCell, GameObject a_nextCell)
-    {
-        a_myCell = myCell;
-        a_nextCell = nextCell;
-    }*/
-    /*  public void setPosIn2DMatrix(int a_xPosInMap,int a_yPosInMap)
-      {
-          xPosInMap = a_xPosInMap;
-          yPosInMap = a_yPosInMap;
-      }*/
-    /*public void doAction(GameObject a_myCell, GameObject a_nextCell)
-    {
-        myCell= a_myCell;
-        nextCell = a_nextCell;
-        startPosition3D = myCell.GetComponent<Cell>().transform.position;
-        destinyPosition3D = nextCell.GetComponent<Cell>().transform.position;
-        //destinyPosition3D = startPosition3D+ Vector3.forward * moveDistance;
-        startMoveCounter = true;
-        move();
-       
-    }*/
+    }
     public void doAction()
     {
-        startPosition3D = transform.position;
-        destinyPosition3D = startPosition3D + Vector3.forward * moveDistance;
-        startMoveCounter = true;
+        switch (myCurrentAction)
+        {
+            case actionsToDo.moveAction:
+                startPosition3D = transform.position;
+                destinyPosition3D = startPosition3D + Vector3.forward * moveDistance;
+                startMoveCounter = true;
+                break;
+        }
+
     }
     void move()
     {
-        if (moveCharacterTimer< moveCharacterTime)
+        if (moveCharacterTimer < moveCharacterTime)
         {
-            
             float moveCharacterTimerNormalized = moveCharacterTimer % moveCharacterTime;
             Vector3 vec3Lerp = Vector3.Lerp(startPosition3D, destinyPosition3D, moveCharacterTimer);
-            //  float increment =;
-            //transform.Translate(Vector3.forward * Time.deltaTime * vec3Lerp.magnitude);
-            transform.position=vec3Lerp;
-          //  print(vec3Lerp);
+            transform.position = vec3Lerp;
         }
         else
         {
             moveCharacterTimer = 0;
             startMoveCounter = false;
         }
-        
     }
-    void Start () {
-
-        myDisability = disabilites.blind;
-
-    }
-	
-
-    
-	// Update is called once per frame
-	void Update () {
-        if (startMoveCounter)
+    void rotate()
+    {
+        if (moveCharacterTimer < moveCharacterTime)
         {
-            move();
-            moveCharacterTimer += Time.deltaTime;
+            float rotateCharacterTimerNormalized = moveCharacterTimer % moveCharacterTime;
+            Transform toRotation= transform;
+            toRotation.Rotate(Vector3.up * Time.deltaTime, Space.World);
+            transform.rotation = Quaternion.Lerp(transform.rotation, transform.rotation, rotateCharacterTimerNormalized);
+           // transform.position = vec3Lerp;
         }
-      
+        else
+        {
+            moveCharacterTimer = 0;
+            startMoveCounter = false;
+        }
     }
 }
--- Assets/Scripts/GameMainController.cs
+++ Assets/Scripts/GameMainController.cs
@@ -6,13 +6,13 @@ public class GameMainController : MonoBehaviour
 {
     public GameObject characterPrefab;
     public GameObject cellPrefab;
-    public GameObject mapPrefab;
-    //vec2Int spawnerlCellMatrixPos;
+    public GameObject spawnerGameObject;
+    // parents no hace falta que sean prefabs ya que genera algo de tipo empty
 
-    public GameObject mapInstantiationParent; // to store the map tiles here
-    GameObject spawnCell;
-    Vector2 spawnCellPosition3D; // se puede sacar a partir de la 2d, si se tiene una variable que sea el tamaño de cada casilla
-    Vector2 spawnCellPosition2DMatrix = new Vector2(3, 2);
+    public float cellSize = 1;
+    int numCharactersSaved = 0;
+    GameObject mapInstantiationParent; // to store the map tiles here
+    GameObject characterInstantiationParent; // to store the characters here
     List<GameObject> characters;
     int xColumns = 5; /// num columns
     int yRows = 5;  // num rows
@@ -21,47 +21,30 @@ public class GameMainController : MonoBehaviour
 
     float doActionTime = 2;
     float doActionTimer = 0;
-
-    float cellSize = 1;
-    float instantiationHeightCharacter = 0.6f;
-    GameObject[,] map;
     // podemos hacer una matriz ya que en ningun momento va a haber 2 characters en una misma casilla
-    GameObject[,] charactersMapPosition;
-
-    //GameObject cell;
-    //Cell[,] map = new Cell[height, width];
-
-    // Use this for initialization
-
     void Start()
     {
+        characterInstantiationParent = Instantiate(new GameObject(), new Vector3(0, 0, 0), Quaternion.identity);
+        characterInstantiationParent.name = "Characters";
         characters = new List<GameObject>();
-        map = new GameObject[yRows, xColumns];
         createMap();
-       
-        spawnCell = Instantiate(cellPrefab,
+        spawnerGameObject = Instantiate(cellPrefab,
             new Vector3(2,0,0), Quaternion.identity);
         createCharacterTimer = createCharacterTime; // para que no tarde el tiempo maximo en instanciarlo
     }
     void createCharacter()
     {
-        /*
-        //print(spawnCellPosition2DMatrix);
-        Cell cellScript = spawnCell.GetComponent<Cell>();
-        GameObject createdCharacter = Instantiate(characterPrefab, 
-            new Vector3(spawnCell.transform.position.x, spawnCell.transform.position.y+cellScript.characterPosYCell, spawnCell.transform.position.z), Quaternion.identity);
-        Character characterScript=createdCharacter.GetComponent<Character>();
-        characters.Add(createdCharacter);
-        */
-        Cell cellScript = spawnCell.GetComponent<Cell>();
+        Vector3 positionSpawner = spawnerGameObject.transform.position;
         GameObject createdCharacter = Instantiate(characterPrefab,
-            new Vector3(spawnCell.transform.position.x, spawnCell.transform.position.y + cellScript.characterPosYCell, spawnCell.transform.position.z), Quaternion.identity);
-        Character characterScript = createdCharacter.GetComponent<Character>();
+            new Vector3(positionSpawner.x, positionSpawner.y, positionSpawner.z), Quaternion.identity);
+       // Character characterScript = createdCharacter.GetComponent<Character>();
+        createdCharacter.transform.SetParent(characterInstantiationParent.transform, false);
         characters.Add(createdCharacter);
     }
     void createMap()
     {
-        /*mapInstantiationParent = Instantiate(mapPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        mapInstantiationParent = Instantiate(new GameObject(), new Vector3(0, 0, 0), Quaternion.identity);
+        mapInstantiationParent.name = "Map";
         float cellPosY = 0;
         for (int r = 0; r < xColumns; r++)
         {
@@ -71,26 +54,6 @@ public class GameMainController : MonoBehaviour
                 cellPosX += cellSize;
                 GameObject createdCellObject = Instantiate(cellPrefab, new Vector3(cellPosX, 0, cellPosY), Quaternion.identity);
                 createdCellObject.transform.SetParent(mapInstantiationParent.transform, false);
-                Cell cellScript = createdCellObject.GetComponent<Cell>();
-                // print("hola");
-                cellScript.setPos2DMatrix(r,c);
-                //cellScript.position = new Vec3(r.xPos,0 ,c);
-                 map[r, c] = createdCellObject;
-            }
-            cellPosY += cellSize;
-        }
-        */
-        mapInstantiationParent = Instantiate(mapPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-        float cellPosY = 0;
-        for (int r = 0; r < xColumns; r++)
-        {
-            float cellPosX = 0;
-            for (int c = 0; c < yRows; c++)
-            {
-                cellPosX += cellSize;
-                GameObject createdCellObject = Instantiate(cellPrefab, new Vector3(cellPosX, 0, cellPosY), Quaternion.identity);
-                createdCellObject.transform.SetParent(mapInstantiationParent.transform, false);
-                map[r, c] = createdCellObject;
             }
             cellPosY += cellSize;
         }
@@ -101,35 +64,22 @@ public class GameMainController : MonoBehaviour
         foreach (GameObject guy in characters)
         {
             Character character = guy.GetComponent<Character>();
-            print(character.myDisability);
+            print(character.getMyDisability());
         }
     }
     void callToAction()
     {
-        /* foreach (GameObject guy in characters)
-         {
-             Character character = guy.GetComponent<Character>();
-             GameObject characterCell = map[character.xPosInMap, character.yPosInMap];
-             int nextCellIncrementX= characterCell.GetComponent<Cell>().xDirection;
-             int nextCellIncrementY = characterCell.GetComponent<Cell>().yDirection;
-             int nextCellPosX = character.xPosInMap + nextCellIncrementX;
-             int nextCellPosY = character.yPosInMap + nextCellIncrementY;
-             if (nextCellPosX>=xColumns|| nextCellPosY >= yRows){
-                 Debug.Log("Error! the next cell is outside boundaries");
-             }
-             GameObject characterNextCell = map[nextCellPosX, nextCellPosY];
-             print("Going from "+ character.xPosInMap+", "+ character.yPosInMap+
-                 " to "+nextCellPosX + ", " + nextCellPosY);
-
-             character.doAction(characterCell, characterNextCell);
-         }*/
-
+        showDisabilities();
         foreach (GameObject guy in characters)
         {
             Character character = guy.GetComponent<Character>();
             character.doAction();
         }
 
+    }
+    public void characterSaved()
+    {
+        numCharactersSaved++;
     }
     // Update is called once per frame
     void Update()
@@ -145,12 +95,7 @@ public class GameMainController : MonoBehaviour
             doActionTimer = 0;
             callToAction();
         }
-
         doActionTimer += Time.deltaTime;
-
-
-
-
     }
 }
 
