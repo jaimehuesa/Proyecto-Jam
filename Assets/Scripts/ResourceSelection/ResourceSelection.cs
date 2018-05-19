a690fb65890cf585e3ab9c7344b4befa730f34ad
jaimehuesa <jaime.huesa@gmail.com>
2018-05-19 13:28:28 +0200

    UISeleccion de soluciones

--- Assets/Scripts/ResourceSelection/ResourceSelection.cs
+++ Assets/Scripts/ResourceSelection/ResourceSelection.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class ResourceSelection : MonoBehaviour {
 	public GameObject[] solutions;
+
+	public GameObject solution1UI;
+	public GameObject solution2UI;
+	public GameObject solution3UI;
+
 	[HideInInspector]
 	public GameObject currentSolution;
+	private GameObject currentSolutionUI;
+
 	// Use this for initialization
 	void Start () {
 		currentSolution = solutions [0];
+		solution1UI.SetActive (true);
+		currentSolutionUI = solution1UI;
 		Debug.Log ("Primera solucion seleccionada");
 	}
 	
@@ -16,14 +25,23 @@ public class ResourceSelection : MonoBehaviour {
 	void Update () {
 		if(Input.GetKey("1")){
 			currentSolution = solutions [0];
+			currentSolutionUI.SetActive (false);
+			solution1UI.SetActive (true);
+			currentSolutionUI = solution1UI;
 			Debug.Log("Primera solucion seleccionada");
 		}
 		if(Input.GetKey("2")){
 			currentSolution = solutions [1];
+			currentSolutionUI.SetActive (false);
+			solution2UI.SetActive (true);
+			currentSolutionUI = solution2UI;
 			Debug.Log("Segunda solucion seleccionada");
 		}
 		if(Input.GetKey("3")){
 			currentSolution = solutions [2];
+			currentSolutionUI.SetActive (false);
+			solution3UI.SetActive (true);
+			currentSolutionUI = solution3UI;
 			Debug.Log("Tercera solucion seleccionada");
 		}
 
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void EnableUIObject(GameObject go){
+		go.SetActive (true);
+	}
+
+	public void DisableUIObject(GameObject go){
+		go.SetActive (false);
+	}
+
+}
