c0fe21fab3c6adf20dd084e57c586a4b8a8bf6ed
irx99 <adriang.gtb@gmail.com>
2018-05-19 03:17:16 +0200

    Añadida clase Cell padre

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -2,16 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cell : MonoBehaviour {
+public abstract class Cell : MonoBehaviour {
 
-    // Use this for initialization
-    
-    void Start () {
-		
+    /// <summary>
+	/// 0 if the cell has no functionality,
+	/// 1 if is a turn cell and 2 if is an obstacle
+	/// </summary>
+	protected int cellType;
+	/// <summary>
+	/// -1 if is not a turn cell
+	/// 0 for left turn,
+	/// 1 for right turn
+	/// </summary>
+	protected int turnTo;
+
+	public void ReplaceCell(GameObject newCell){
+		Instantiate(newCell, this.transform.position, Quaternion.identity);
+		Destroy(this.gameObject);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	/// <summary>
+	/// Returns 0 if the cell has no functionality,
+	/// 1 if is a turn cell and 2 if is an obstacle
+	/// </summary>
+	public int GetCellType(){
+		return cellType;
+	}
+
+	/// <summary>
+	/// Returns true if the disabled person type can overcome this cell
+    /// else returns false
+	/// </summary>
+	/// <param name="disabledType"> Disability type -> 1 WheelChair, 2 Blind, 3 Alzheimer.</param>
+	public abstract bool CanBeOvercome(int disabledType);
+
+	/// <summary>
+	/// Returns true if the disabled person has to turn left
+    /// else returns false (right turn)
+	/// </summary>
+	public bool HasToTurnLeft(){
+		if(turnTo == -1){
+			Debug.LogError("This is not a turn cell!");
+			return true;
+		}else if(turnTo == 0){
+			return true;
+		}else{
+			return false;
+		}
 	}
 }
---
+++ Assets/Scripts/Cells Type/WheelchairObstacleCell.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelchairObstacleCell : Cell {
+
+	// Use this for initialization
+	void Start () {
+		cellType = 2;
+		turnTo = -1;
+	}
+	
+	public override bool CanBeOvercome(int dysabledType){
+		if(dysabledType == 1){
+			return false;
+		}else{
+			return true;
+		}
+	}
+}
