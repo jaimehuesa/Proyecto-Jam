d3d1b3d029131daff50054a3b90b6a7ca173692b
irx99 <adriang.gtb@gmail.com>
2018-05-20 16:24:13 +0200

    Tutorial funcionando

--- Assets/Scripts/TutorialScene.cs
+++ Assets/Scripts/TutorialScene.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TutorialScene : MonoBehaviour {
 
+	private bool firstScene = true;
+	public Sprite secondImage;
+	public Image imagePosition;
+
 	void Update(){
 		if(Input.GetMouseButtonDown(0)){
-			SceneManager.LoadScene("Main");
+			if(!firstScene){
+				SceneManager.LoadScene("Main");
+			}else{
+				imagePosition.sprite = secondImage;
+				firstScene = false;
+			}			
 		}
 	}
 }
