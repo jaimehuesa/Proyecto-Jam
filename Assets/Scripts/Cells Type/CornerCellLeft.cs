9f87cf5ab64ec52093241fefca9b55751f780d29
jaimehuesa <jaime.huesa@gmail.com>
2018-05-20 12:43:56 +0200

    Corners Cells

---
+++ Assets/Scripts/Cells Type/CornerCellLeft.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornerCellLeft : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider c){
+		if (c.CompareTag ("Character")) {
+			c.GetComponent<Character> ().callRotateFromCell (-90f);
+		}
+	}
+}
